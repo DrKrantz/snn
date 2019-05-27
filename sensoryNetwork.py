3d8c363c6114e52b623399a9bf5db627fcbba556
staude <staude@architrave.de>
2019-05-27 16:05:35 +0200

    Code beauty
     - sensory network

--- sensoryNetwork.py
+++ sensoryNetwork.py
@@ -11,9 +11,8 @@ NEEDS:
 """
 
 from numpy import *
-from numpy import ones,zeros,nonzero,sum,shape
+from numpy import ones, zeros, nonzero, sum, shape
 import time
-import os
 import pygame
 import pygame.locals
 import sys
@@ -26,6 +25,7 @@ import inputDevices
 from connectivityMatrix import ConnectivityMatrix
 import settingsReader
 
+
 class SensoryNetwork(object):
     def __init__(self, inputHandler, outputHandler, pars, connectivityMatrix):
         super(SensoryNetwork, self).__init__()
@@ -33,46 +33,34 @@ class SensoryNetwork(object):
         self.pars = pars
         self.outputHandler = outputHandler
 
-        # value = raw_input('setup ok? [y]/n \n')
-        # if value == 'n':
-        #     return
-
         self.__A = connectivityMatrix
         
-        N = self.pars['N']
-        #vectors with parameters of adaptation and synapses
-        self.__a = ones(N)
+        n = self.pars['N']
+        # vectors with parameters of adaptation and synapses
+        self.__a = ones(n)
         self.__a[self.pars['Exc_ids']] = self.pars['a_e']
         self.__a[self.pars['Inh_ids']] = self.pars['a_i']
-        self.__b = ones(N)
+        self.__b = ones(n)
         self.__b[self.pars['Exc_ids']] = self.pars['b_e']
         self.__b[self.pars['Inh_ids']] = self.pars['b_i']
-        self.__ge = self.pars['s_e']*ones((N))  # conductances of excitatory synapses
-        self.__gi = self.pars['s_i']*ones((N))  # conductances of inhibitory synapses
+        self.__ge = self.pars['s_e']*ones(n)  # conductances of excitatory synapses
+        self.__gi = self.pars['s_i']*ones(n)  # conductances of inhibitory synapses
         
-        self.__v = self.pars['EL']*ones((N))  # Initial values of the mmbrane potential v
+        self.__v = self.pars['EL']*ones(n)  # Initial values of the mmbrane potential v
         self.__w = self.__b  # Initial values of adaptation variable w
         self.__neurons = array([])   # neuron IDs
         self.__hasPrinted = False
         self.deaddur = array([])  # duration (secs) the dead neurons have been dead
-        self.deadIDs = array([],int)
+        self.deadIDs = array([], int)
         
-    def update(self): # spike times ):
-
-        # t += 1
-        # if t == 20:
-        #     output = open('networkPars.pkl', 'wb')
-        #     pickle.dump(self.pars, output)
-        #     print 'parameters saved'
-        #     output.close()
-
-        ##### GET WEBCAM IMAGE, UPDATE VIEWER & INPUTS ###########
+    def update(self):
+        # GET WEBCAM IMAGE, UPDATE VIEWER & INPUTS ###########
         self.inputHandler.update()
         self.pars.update(self.inputHandler.getPars())
         external = self.pars['midi_external']
         self.outputHandler.turnOff()
 
-        ########## UPDATE DEADIMES AND GET FIRED IDs  ###########
+        # UPDATE DEADIMES AND GET FIRED IDs  ###########
         # update deadtimes
         self.deaddur += self.pars['h']    # increment the time of the dead
         aliveID = nonzero(self.deaddur > self.pars['dead'])[0]
@@ -83,32 +71,21 @@ class SensoryNetwork(object):
         fired = nonzero(self.__v >= self.pars['threshold'])[0]
         self.deadIDs = concatenate((self.deadIDs, fired))  # put fired neuron to death
         self.deaddur = concatenate((self.deaddur, zeros(shape(fired))))
-        #spiketimes=concatenate((spiketimes,t*pars['h']*1000+0*fired))
-        #neurons = concatenate((neurons,fired+1))
         extFired = self.inputHandler.getFired()
-        # if len(extFired)>0:
-        #     print 'fired', extFired
 
         fired = array(union1d(fired, extFired), int)
 
-
-        # print 'fired: vor', fired, type(fired), 'nach', postfired, type(fired)
         self.__v[fired] = self.pars['EL']  # set spiked neurons to reset potential
-        self.__w[fired] += self.__b[fired]  # increment adaptation variable of
-                                            # fired neurons
+        self.__w[fired] += self.__b[fired]  # increment adaptation variable of fired neurons
 
-#            allfired.extend(fired)
-#            alltimes.extend(ones(shape(fired))*t*pars['h'])
-
-        #### SEND TO HANDLER ###
+        # SEND TO HANDLER ###
         self.outputHandler.update(fired)
-        # self.outputHandler.updateObject(extFired)
 
         # update conductances of excitatory synapses
         fired_e = intersect1d(fired, self.pars['Exc_ids'])  # spiking e-neurons
         nPreSp_e = sum(self.__A[:, fired_e], axis=1)  # number of presynaptic e-spikes
         self.__ge += -self.pars['h'] * self.__ge/self.pars['tau_e'] + \
-                    nPreSp_e * self.pars['s_e']
+                     nPreSp_e * self.pars['s_e']
 
         # update conductances of inh. synapses
         fired_i = intersect1d(fired, self.pars['Inh_ids'])  # spiking i-neurons
@@ -116,18 +93,16 @@ class SensoryNetwork(object):
         self.__gi += -self.pars['h'] * self.__gi/self.pars['tau_i'] + \
                     nPreSp_i * self.pars['s_i']
 
-        #update membrane and adaptation variable
-        self.__v += self.pars['h']*(-self.pars['gL']*(self.__v-self.pars['EL']) + \
-              self.pars['gL']*self.pars['Delta']*exp((self.__v-self.pars['threshold'])/self.pars['Delta']) \
-              -self.__w/self.pars['S'] - self.__ge*(self.__v-self.pars['Ee'])/self.pars['S'] \
-              -self.__gi*(self.__v-self.pars['Ei'])/self.pars['S'] )/self.pars['Cm'] \
-              + self.pars['h']*external/self.pars['Cm']
+        # update membrane and adaptation variable
+        self.__v += self.pars['h']*(-self.pars['gL']*(self.__v-self.pars['EL']) +
+              self.pars['gL']*self.pars['Delta']*exp((self.__v-self.pars['threshold'])/self.pars['Delta']) -
+              self.__w/self.pars['S'] - self.__ge*(self.__v-self.pars['Ee'])/self.pars['S'] -
+              self.__gi*(self.__v-self.pars['Ei'])/self.pars['S'])/self.pars['Cm'] + \
+              self.pars['h']*external/self.pars['Cm']
         self.__v[self.deadIDs] = self.pars['EL']  # clamp dead neurons to resting potential
         self.__w += self.pars['h'] * (self.__a * (self.__v - self.pars['EL']) -
                                       self.__w)/self.pars['tau_w']
-        # print 'g_e', self.__ge
-        # print 'g_i', self.__gi
-        # raw_input('ok?')
+
 
 class DeviceManager:
     def __init__(self, devices, pars):
@@ -135,12 +110,10 @@ class DeviceManager:
         self.pars = pars
         self.inputs = {}
         self.outputs = {}
-        # print self.devices
         self.__createInputDevices(pars)
         self.__createOutputDevices()
 
     def __createInputDevices(self, pars):
-        inputs = {}
         for devicename, midiport in self.deviceSettings['inputs'].items():
             self.inputs[devicename] = getattr(inputDevices, devicename)(midiport, pars)
 
@@ -151,11 +124,12 @@ class DeviceManager:
     def getInputDevices(self):
         return list(self.inputs.values())
 
+
 class MainApp:
     def __init__(self, deviceManager, pars):
         self.__fullscreen = False
         pygame.init()
-        self.pars=pars
+        self.pars = pars
         self.keyboardInput = deviceManager.inputs['KeyboardInput']
 
         inputHandler = InputHandler(
@@ -172,7 +146,7 @@ class MainApp:
         if self.network is not None:
             self.run()
 
-    def input(self,events):
+    def input(self, events):
         for event in events:
             if event.type == pygame.locals.QUIT:
                 sys.exit(0)
@@ -180,7 +154,7 @@ class MainApp:
                 if event.dict['key'] == pygame.locals.K_ESCAPE:
                     sys.exit(0)
                 elif event.dict['key'] == pygame.locals.K_f:
-                    self.__fullscreen = not(self.__fullscreen)
+                    self.__fullscreen = not self.__fullscreen
                     if self.__fullscreen:
                         pygame.display.set_mode(
                             self.network.outputHandler.display.spikeScreenSize,
@@ -215,7 +189,7 @@ class MainApp:
         lastUpdated = time.time()
         while True:
             self.input(pygame.event.get())
-            if time.time()-lastUpdated<self.pars['pause']:
+            if time.time()-lastUpdated < self.pars['pause']:
                 pass
             else:
                 self.network.update()
