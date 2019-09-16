60a5d520218be91311882d47bb4fe7a7579732fe
staude <stoide@googlemail.com>
2019-09-16 15:18:15 +0200

    Send spikes via osc

--- outputHandler.py
+++ outputHandler.py
@@ -1,5 +1,6 @@
 import pygame.font
 from numpy import intersect1d
+from pythonosc.udp_client import SimpleUDPClient
 
 from display import Display
 from outputDevices import *
@@ -7,37 +8,37 @@ global pars
 
 
 class OutputHandler(object):
+    ADDRESS_SOUND = "/sound"
+    IP = "127.0.0.1"
+    PORT = 1337
+
     def __init__(self, outputs, pars, neuron2NoteConversion=4):
-        self.pars = pars
         super(OutputHandler, self).__init__()
+        self.__output = outputs
+        self.pars = pars
+        self.__neuron2NoteConversion = neuron2NoteConversion
+
         self.display = Display(pars['N_col'], pars['N_row'],
                                ['Ne', 'Ni', 's_e', 's_i', 'tau_e', 'tau_i', 'midi_ext_e', 'midi_ext_i',
                                 'cam_ext', 'cam_external_max'], 'lines', screenSize=pars['screen_size'])
-        pm.init()
-        self.__output = outputs
-        self.__input = {}
 
-        if Visuals.NAME in self.__output:
+        self.__client = SimpleUDPClient(self.IP, self.PORT)
+        pm.init()
+
+        if Visuals.NAME in self.__output:  # TODO: why is this here?
             self.__output[Visuals.NAME].note_on(1)
 
         self.__now = time.time()
         self.__activeNotes = set()
-        self.__neuron2NoteConversion = neuron2NoteConversion
-
-    def __setupInputs(self, inputList):
-        for name in inputList:
-            self.__input[name] = \
-                self.__getDevice(self.__name2Identifier[name], type='input')
 
     def update(self, fired):
         neuron_ids = intersect1d(fired, self.pars['note_ids'])
+
         self.__checkKeyChange(neuron_ids)
 
-        n_fired = neuron_ids.__len__()
-        if n_fired > 0:
-            for neuron_id in neuron_ids:
-                for name, output in self.__output.items():
-                    output.note_on(neuron_id)
+        if len(neuron_ids) > 0:
+            ids = [int(i) for i in neuron_ids]  # TODO: I'm sure there's a smarter way to fix this
+            self.__client.send_message(self.ADDRESS_SOUND, ids)
                 
         # display spikes and update display
         self.display.update_fired(fired)
