ca63406ebb017cf149288bc43db6530b6d413ae3
staude <stoide@googlemail.com>
2020-01-20 17:26:37 +0100

    avoid computation of large x_values
     by using periodicity of sine

--- instrument.py
+++ instrument.py
@@ -71,6 +71,7 @@ class OscInstrument:
     __indices_to_update = []
     __n_freqs = 0
     __data_matrix = np.array([])
+    __x_data = np.array([])
     __max_volume = .85 * 32767
 
     def __init__(self, notes=None):
@@ -96,13 +97,16 @@ class OscInstrument:
         self.__n_freqs = len(notes)
         self.__volumes = np.ones_like(self.__frequencies)*self.__max_volume
         self.__data_matrix = np.ones((self.__n_freqs, self.CHANNELS * self.CHUNK))
+        #  every x-value needs to be duplicated for stereo sound
+        duplicated_xrange = np.resize(np.arange(self.CHUNK), (2, self.CHUNK)).T.flatten()
+        self.__x_data = np.mat(self.__frequencies).T * duplicated_xrange * 2 * np.pi / self.RATE
 
     def _volume_decay(self):
         self.__volumes *= self.DECAY_FACTOR
 
-    def _compute_current_signal(self, x_data):
+    def _compute_current_signal(self):
         # a (num-frequencies, num-samples)-matrix, where the kth row is the sine wave for the kth neuron
-        base_signal = np.cos(np.mat(self.__frequencies).T * x_data * 2 * np.pi / self.RATE)
+        base_signal = np.sin(self.__x_data)
         for freq_idx, wave_form in enumerate(np.array(base_signal)):
 
             # detect the 'switch_idx' where the signal crosses zero
@@ -126,9 +130,16 @@ class OscInstrument:
 
         return np.mean(self.__data_matrix, 0)
 
-    def _shift_x_data(self, x_values):
-        # TODO: reset x_data to start from 0 at some point to avoid too large numbers
-        return x_values + self.CHUNK
+    def _shift_x_data(self):
+        x_data = np.ones_like(self.__x_data)
+        for freq_idx, x in enumerate(np.array(self.__x_data)):
+            # first shift x
+            x += self.CHUNK*2*np.pi*self.__frequencies[freq_idx]/self.RATE
+            # compute the multiple of 2pi that can be subtracted
+            factor = np.floor(x[0]/(2*np.pi))
+            # shift x_data to the left
+            x_data[freq_idx, :] = x-factor*2*np.pi
+        self.__x_data = x_data
 
     def _update_volume(self, note):
         self.__indices_to_update.append(self.__notes.index(note))
@@ -141,25 +152,22 @@ class OscInstrument:
             self._update_volume(content)
 
     async def loop(self):
-        #  every x-value needs to be duplicated for stereo sound
-        x_data = np.resize(np.arange(self.CHUNK), (2, self.CHUNK)).T.flatten()
-
         all_data = np.array([])
         while True:
             if len(self.__frequencies):
                 # self._volume_decay()
-                data = self._compute_current_signal(x_data)
+                data = self._compute_current_signal()
                 self.__indices_to_update = []
                 self.__stream.write(data.astype(np.int16).tostring())
-                x_data = self._shift_x_data(x_data)
+                self._shift_x_data()
 
-                all_data = np.concatenate([all_data, data])
-                if len(all_data) >= 100*self.CHUNK:
-                    with open('test.pkl', 'wb') as f:
-                        pickle.dump(all_data, f)
+                # all_data = np.concatenate([all_data, data])
+                # if len(all_data) >= 100*self.CHUNK:
+                #     with open('test.pkl', 'wb') as f:
+                #         pickle.dump(all_data, f)
                     # break
 
-            await asyncio.sleep(1000/(4*self.RATE))
+            await asyncio.sleep(1000/(2*self.RATE))
 
     async def init_main(self):
         dispatcher = Dispatcher()
