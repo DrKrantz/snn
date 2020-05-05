78358459d0a8c28ed306d71af8f4d9999a00efa4
staude <stoide@googlemail.com>
2020-05-05 21:56:57 +0200

    Use osc-config in nest-forwarder

--- nest-code/nest-terminal-osc-forwarder.py
+++ nest-code/nest-terminal-osc-forwarder.py
@@ -4,37 +4,35 @@ from subprocess import Popen, PIPE
 from pythonosc.udp_client import SimpleUDPClient
 
 
-address = '/spikes'
-
-
 class OSCForwarder:
-    def __init__(self, client, simlator_script='brunel_delta_nest.py'):
+    def __init__(self, client, address, simlator_script='nest-code/brunel_delta_nest.py'):
         self.__client = client
+        self.__address = address
         self.__recorder = Popen(['python3', simlator_script], stdout=PIPE)
         self.buffer = b''
 
     def run(self):
-
         while True:
             out = self.__recorder.stdout.read(1)
 
             if out == b'\n':
                 if self.buffer.find(b'\t') > 0:
                     neuron, spiketime = self.buffer.split(b'\t')
-                    client.send_message(address, int(neuron))
+                    self.__client.send_message(self.__address, int(neuron))
                     # print('Receiving: neuron {} at time {}'.format(
                     #     int(neuron), float(spiketime)))
                 else:
-                    print(str(self.buffer))
+                    print(self.buffer.decode('utf-8'))
                 self.buffer = b''
             else:
                 self.buffer += out
 
 
 if __name__ == '__main__':
-    ip = '192.168.33.1'
-    port = 8080
-    client = SimpleUDPClient(ip, port)
+    import config_parser
+    ip = config_parser.config['sources']['simulator']['ip']
+    port = config_parser.config['sources']['simulator']['port']
+    osc_client = SimpleUDPClient(ip, port)
 
-    forwarder = OSCForwarder(client)
+    forwarder = OSCForwarder(osc_client, config_parser.config['sources']['simulator']['address'])
     forwarder.run()
