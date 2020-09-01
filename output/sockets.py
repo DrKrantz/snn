bdffbdb5b16e5c8c9a6df6c08997916c4ed76e3b
staude <stoide@googlemail.com>
2020-09-01 14:29:38 +0200

    Force little endian formatting

--- output/sockets.py
+++ output/sockets.py
@@ -24,7 +24,7 @@ class SpikeSocket(socket.socket):
         super(SpikeSocket, self).__init__(socket.AF_INET, socket.SOCK_DGRAM, *args, **kwargs)
 
     def send_converted(self, neuron_id):
-        self.sendto(struct.pack('HH', MESSAGETYPE_PERFORMANCE, self.convert(neuron_id)), self.target_address)
+        self.sendto(struct.pack('<bH', MESSAGETYPE_PERFORMANCE, self.convert(neuron_id)), self.target_address)
 
 
 class SpikeForwarder(socket.socket):
@@ -41,15 +41,14 @@ class SpikeForwarder(socket.socket):
         self.targets.append(target)
 
     def run(self):
-        self.bind(self.receiving_address)
-        self.start_init()
         self.start_forwarding()
 
     def start_init(self):
+        self.bind(self.receiving_address)
         while True:
             data, addr = self.recvfrom(MESSAGESIZE_INIT_FORWARDER)
             if data:
-                msg_type, content = struct.unpack('HH', data)
+                msg_type, content = struct.unpack('<HH', data)
                 print('received type {} with content {}'.format(msg_type, content))
                 if msg_type == MESSAGETYPE_TOTAL_NEURONS:
                     self.n_neurons = content
@@ -73,11 +72,10 @@ class SpikeForwarder(socket.socket):
                     [target.send_converted(neuron_id) for target in self.targets]
 
     def __validate_performance_msg(self, data):
-        msg_type, neuron_id = struct.unpack('HH', data)
+        msg_type, neuron_id = struct.unpack('<HH', data)
         if neuron_id not in self.__neuron_ids:
             msg_type = MESSAGETYPE_ERROR
         return msg_type, neuron_id
-
 
 
 class InitSocket(socket.socket):
@@ -86,6 +84,6 @@ class InitSocket(socket.socket):
         super(InitSocket, self).__init__(socket.AF_INET, socket.SOCK_DGRAM, *args, **kwargs)
 
     def send_init(self, frequencies):
-        self.sendto(struct.pack('bHf', MESSAGETYPE_INIT_INSTRUMENT, len(frequencies), 0.0), self.target_address)
+        self.sendto(struct.pack('<bHf', MESSAGETYPE_INIT_INSTRUMENT, len(frequencies), 0.0), self.target_address)
         for idx, freq in enumerate(frequencies):
-            self.sendto(struct.pack('bHf', MESSAGETYPE_INIT_INSTRUMENT, idx, freq), self.target_address)
+            self.sendto(struct.pack('<bHf', MESSAGETYPE_INIT_INSTRUMENT, idx, freq), self.target_address)
